6d0130506566dfb749996ee6d6a1a900c19aa325
witiz <contact@witiz.com>
2024-09-26 12:19:31 +0200

    Add dynamic version and bump to 0.9.4

--- src/markupy/__init__.py
+++ src/markupy/__init__.py
@@ -3,3 +3,4 @@ from ._private.html import to_markupy as html2markupy
 from ._private.view import Node, View, iter_node, render_node
 
 __all__ = ["Element", "Node", "View", "html2markupy", "iter_node", "render_node"]
+__version__ = "0.9.4"
