af7ce32abc35c4934bc3106ba40bc144713b26dd
witiz <contact@witiz.com>
2025-03-30 12:23:01 +0200

    Components need to render a View

--- src/markupy/_private/component.py
+++ src/markupy/_private/component.py
@@ -1,15 +1,14 @@
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterator
-from typing import Any, final
+from typing import final
 
-from .fragment import Fragment
 from .view import View
 
 
 class Component(View, metaclass=ABCMeta):
     @abstractmethod
-    def render(self) -> Any: ...
+    def render(self) -> View: ...
 
     @final
     def __iter__(self) -> Iterator[str]:
-        yield from Fragment(self.render())
+        yield from self.render()
