0d759eab47ed8e9713c7b1939663222c27592a9e
witiz <contact@witiz.com>
2025-03-31 09:29:05 +0200

    Update performance test

--- scripts/performance.py
+++ scripts/performance.py
@@ -1,21 +1,20 @@
 import cProfile
 
+from markupy import View
 from markupy.tag import Table, Tbody, Td, Th, Thead, Tr
 
 rows = list(range(50_000))
 
 
-def render_markupy_attr() -> str:
-    return str(
-        Table[
-            Thead[Tr[Th["Row #"]]],
-            Tbody[(Tr(".row")[Td(dataValue=row)[row]] for row in rows)],
-        ]
-    )
+def render() -> View:
+    return Table[
+        Thead[Tr[Th["Row #"]]],
+        Tbody[(Tr(".row")[Td(dataValue=row)[row]] for row in rows)],
+    ]
 
 
 with cProfile.Profile() as pr:
-    render_markupy_attr()
+    str(render())
     # Results can be wiewed with snakeviz
     # uvx snakeviz output.prof
     pr.dump_stats("output.prof")
