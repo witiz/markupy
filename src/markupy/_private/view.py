c1b240dcd2395b176df9ffd1363ab18611fce67f
witiz <contact@witiz.com>
2025-04-06 18:45:04 +0200

    Add view constructor for empty view creation

--- src/markupy/_private/view.py
+++ src/markupy/_private/view.py
@@ -1,8 +1,11 @@
-from collections.abc import Iterable
+from collections.abc import Iterable, Iterator
 from typing import final
 
 
 class View(Iterable[str]):
+    def __iter__(self) -> Iterator[str]:
+        yield ""
+
     @final
     def __str__(self) -> str:
         return "".join(self)
@@ -12,11 +15,5 @@ class View(Iterable[str]):
     # https://github.com/encode/starlette/blob/5ed55c441126687106109a3f5e051176f88cd3e6/starlette/responses.py#L44-L49
     def encode(self, encoding: str = "utf-8", errors: str = "strict") -> bytes:
         return str(self).encode(encoding, errors)
-
-    # Avoid having Django "call" a markupy element that is injected into a
-    # template. Setting do_not_call_in_templates will prevent Django from doing
-    # an extra call:
-    # https://docs.djangoproject.com/en/5.0/ref/templates/api/#variables-and-lookups
-    do_not_call_in_templates = True
 
     __html__ = __str__
