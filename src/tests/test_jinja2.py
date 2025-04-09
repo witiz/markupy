7c8b5dae13d6d20135325678a72aad44707f7b94
witiz <contact@witiz.com>
2025-04-09 17:33:58 +0200

    Add autoescape to jinja2 test

--- src/tests/test_jinja2.py
+++
@@ -1,8 +0,0 @@
-from jinja2 import Template
-
-from markupy.tag import Li
-
-
-def test_template_injection() -> None:
-    result = Template("<ul>{{ stuff }}</ul>").render(stuff=Li["I am safe!"])
-    assert result == "<ul><li>I am safe!</li></ul>"
--- src/tests/test_template.py
+++ src/tests/test_template.py
@@ -1,22 +1,21 @@
 import typing
 
 from django.template import Context, Engine  # type:ignore
-from jinja2 import Template
+from jinja2 import Environment
 
 from markupy import tag
 
 
 @typing.no_type_check
 def test_django() -> None:
-    template_string = "<div>{{ content }}</div>"
-    template = Engine().from_string(template_string)
+    template = Engine().from_string("<div>{{ content }}</div>")
     context = Context({"content": tag.H1(".title")["django"]})
     rendered = template.render(context)
     assert rendered == """<div><h1 class="title">django</h1></div>"""
 
 
 def test_jinja2() -> None:
-    template_string = "<div>{{ content }}</div>"
-    template = Template(template_string)
+    env = Environment(autoescape=True)
+    template = env.from_string("<div>{{ content }}</div>")
     rendered = template.render(content=tag.H1(".title")["jinja"])
     assert rendered == """<div><h1 class="title">jinja</h1></div>"""
