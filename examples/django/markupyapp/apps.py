22c8b72eebd669535bcaa70fc0920db89f286153
witiz <contact@witiz.com>
2025-04-09 09:54:46 +0200

    Include django example

---
+++ examples/django/markupyapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MarkupyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'markupyapp'
---
+++ examples/django/markupyapp/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from markupy.tag import H1, Body, Html
+
+
+def index(request):
+    return HttpResponse(Html[Body[H1["Hi Django!"]]])
+
+
+def template(request):
+    context = {
+        "content": H1["This is markupy title!"],
+    }
+    return render(request, "markupyapp/base.html", context)
