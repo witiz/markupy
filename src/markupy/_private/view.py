d3f823d18edd4409111736ae99d25f377a473d64
witiz <contact@witiz.com>
2025-04-22 15:29:40 +0200

    Define __eq__ as final

--- src/markupy/_private/view.py
+++ src/markupy/_private/view.py
@@ -19,15 +19,17 @@ class View:
         # to be properly injected in template engines
         return Markup("".join(self))
 
-    def __repr__(self) -> str:
-        return "<markupy.View>"
-
+    @final
     def __eq__(self, other: object) -> bool:
         return str(other) == str(self)
+
+    def __repr__(self) -> str:
+        return "<markupy.View>"
 
     # Allow starlette Response.render to directly render this element without
     # explicitly casting to str:
     # https://github.com/encode/starlette/blob/5ed55c441126687106109a3f5e051176f88cd3e6/starlette/responses.py#L44-L49
+    @final
     def encode(self, encoding: str = "utf-8", errors: str = "strict") -> bytes:
         return str(self).encode(encoding, errors)
 
