b02b1e4a88b6b07d6444f28340791d6426677bc1
witiz <contact@witiz.com>
2025-04-25 18:17:56 +0200

    Expose the register_attribute_handler

--- src/markupy/__init__.py
+++ src/markupy/__init__.py
@@ -1,4 +1,4 @@
-from ._private.attribute import Attribute
+from ._private.attribute import Attribute, register_attribute_handler
 from ._private.component import Component
 from ._private.fragment import Fragment as _Fragment
 from ._private.html import to_markupy as html2markupy
@@ -12,5 +12,6 @@ __all__ = [
     "Fragment",
     "View",
     "html2markupy",
+    "register_attribute_handler",
 ]
-__version__ = "2.0.0"
+__version__ = "2.1.0"
