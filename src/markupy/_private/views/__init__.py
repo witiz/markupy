0c2a7f5c7a4dd92b8f0f9b40d2dedac3330cb765
witiz <contact@witiz.com>
2025-06-01 19:08:52 +0200

    Simplify module

--- src/markupy/_private/views/__init__.py
+++ src/markupy/_private/views/__init__.py
@@ -1,23 +1,12 @@
 from .component import Component
-from .element import (
-    CommentElement,
-    Element,
-    HtmlElement,
-    SafeElement,
-    VoidElement,
-    get_element,
-)
+from .element import Element, get_element
 from .fragment import Fragment
 from .view import View
 
 __all__ = [
     "Component",
+    "Element",
     "Fragment",
     "View",
-    "CommentElement",
-    "Element",
-    "HtmlElement",
-    "SafeElement",
-    "VoidElement",
     "get_element",
 ]
--- src/markupy/elements.py
+++ src/markupy/elements.py
@@ -123,52 +123,28 @@ def __getattr__(name: str) -> _views.Element:
     return _views.get_element(name)
 
 
-_: _views.CommentElement
-
-# Script and Style contents do not need to be wrapped inside Markup()
-Script: _views.SafeElement
-Style: _views.SafeElement
-
-# https://developer.mozilla.org/en-US/docs/Glossary/Doctype
-Html: _views.HtmlElement
-
-# https://developer.mozilla.org/en-US/docs/Glossary/VoidElement
-Area: _views.VoidElement
-Base: _views.VoidElement
-Br: _views.VoidElement
-Col: _views.VoidElement
-Embed: _views.VoidElement
-Hr: _views.VoidElement
-Img: _views.VoidElement
-Input: _views.VoidElement
-Link: _views.VoidElement
-Meta: _views.VoidElement
-Param: _views.VoidElement
-Source: _views.VoidElement
-Track: _views.VoidElement
-Wbr: _views.VoidElement
-
-# Non-deprecated HTML Elements, extracted from
-# https://developer.mozilla.org/en-US/docs/Web/HTML/Element
-# Located via the inspector with:
-# Array.from($0.querySelectorAll('li')).filter(x=>!x.querySelector('.icon-deprecated')).map(x => x.querySelector('code').textContent) # noqa: E501
+_: _views.Element
 A: _views.Element
 Abbr: _views.Element
 Abc: _views.Element
 Address: _views.Element
+Area: _views.Element
 Article: _views.Element
 Aside: _views.Element
 Audio: _views.Element
 B: _views.Element
+Base: _views.Element
 Bdi: _views.Element
 Bdo: _views.Element
 Blockquote: _views.Element
 Body: _views.Element
+Br: _views.Element
 Button: _views.Element
 Canvas: _views.Element
 Caption: _views.Element
 Cite: _views.Element
 Code: _views.Element
+Col: _views.Element
 Colgroup: _views.Element
 Data: _views.Element
 Datalist: _views.Element
@@ -181,6 +157,7 @@ Div: _views.Element
 Dl: _views.Element
 Dt: _views.Element
 Em: _views.Element
+Embed: _views.Element
 Fieldset: _views.Element
 Figcaption: _views.Element
 Figure: _views.Element
@@ -195,17 +172,23 @@ H6: _views.Element
 Head: _views.Element
 Header: _views.Element
 Hgroup: _views.Element
+Hr: _views.Element
+Html: _views.Element
 I: _views.Element  # noqa: E741
 Iframe: _views.Element
+Img: _views.Element
+Input: _views.Element
 Ins: _views.Element
 Kbd: _views.Element
 Label: _views.Element
 Legend: _views.Element
 Li: _views.Element
+Link: _views.Element
 Main: _views.Element
 Map: _views.Element
 Mark: _views.Element
 Menu: _views.Element
+Meta: _views.Element
 Meter: _views.Element
 Nav: _views.Element
 Noscript: _views.Element
@@ -215,6 +198,7 @@ Optgroup: _views.Element
 Option: _views.Element
 Output: _views.Element
 P: _views.Element
+Param: _views.Element
 Picture: _views.Element
 Portal: _views.Element
 Pre: _views.Element
@@ -225,13 +209,16 @@ Rt: _views.Element
 Ruby: _views.Element
 S: _views.Element
 Samp: _views.Element
+Script: _views.Element
 Search: _views.Element
 Section: _views.Element
 Select: _views.Element
 Slot: _views.Element
 Small: _views.Element
+Source: _views.Element
 Span: _views.Element
 Strong: _views.Element
+Style: _views.Element
 Sub: _views.Element
 Summary: _views.Element
 Sup: _views.Element
@@ -246,6 +233,8 @@ Thead: _views.Element
 Time: _views.Element
 Title: _views.Element
 Tr: _views.Element
+Track: _views.Element
 U: _views.Element
 Ul: _views.Element
 Var: _views.Element
+Wbr: _views.Element
